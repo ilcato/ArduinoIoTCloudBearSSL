5ab1609cdf421d1d1a61bee817abeea03b312493
Cristian Maglie <c.maglie@arduino.cc>
2019-02-06 17:57:17 +0100

    Fixed license

--- src/ArduinoIoTCloudBearSSL.cpp
+++ src/ArduinoIoTCloudBearSSL.cpp
@@ -1,25 +1,19 @@
 /*
- * Copyright (c) 2018 Arduino SA. All rights reserved.
+ * This file is part of ArduinoIoTBearSSL.
  *
- * Permission is hereby granted, free of charge, to any person obtaining 
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Copyright 2019 ARDUINO SA (http://www.arduino.cc/)
  *
- * The above copyright notice and this permission notice shall be 
- * included in all copies or substantial portions of the Software.
+ * This software is released under the GNU General Public License version 3,
+ * which covers the main part of ArduinoIoTBearSSL.
+ * The terms of this license can be found at:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * You can be released from the requirements of the above licenses by purchasing
+ * a commercial license. Buying such a license is mandatory if you want to modify or
+ * otherwise use the software for commercial activities involving the Arduino
+ * software without disclosing the source code of your own applications. To purchase
+ * a commercial license, send an email to license@arduino.cc.
+ *
  */
 
 #include "ArduinoIoTCloudBearSSL.h"
